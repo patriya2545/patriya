10081347859f1a65fb827ca1b9db4fc0cf5e54d0
patriya2545 <patriya2545@gmail.com>
2021-04-03 21:04:23 +0700

    1.0.1

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  target:"static",
+  router:{
+    base:"/patriya/"
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
